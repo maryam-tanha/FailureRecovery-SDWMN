bd3fe5bf75197e1ba47eab04c977beac6b602815
Dawood Sajjadi <sajjadi@uvic.ca>
2018-01-10 15:05:21 -0800

    Update enable_bfd.sh

--- enable_bfd.sh
+++ enable_bfd.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # bfd_local_dst_mac represents the mac address of peer interface for wlan0 interface
+# We are thankful to Ricardo Santos (ricardo.santos@kau.se) for sharing his experience on BFD with us.
 INTVAL=10
 ovs-vsctl set interface wlan0 bfd:enable=true
 ovs-vsctl set interface wlan0 bfd:bfd_local_dst_mac=04:f0:21:11:3d:82 -- set interface wlan0 bfd:min_rx=$INTVAL -- set interface wlan0 bfd:min_tx=$INTVAL
